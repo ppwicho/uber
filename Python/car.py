9c0e50109ace1e27bed2fb61bb78c405555adfb4
@ppwicho <luis89@gmail.com>
2021-03-28 18:47:12 -0600

    Agregamos los init y referenciamos entre account y car

--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,11 @@
+from account import Account
 class Car:
-    id=int
-    license=str
-    driver=str
-    passenger=str
+    id          =int
+    license     =str
+    driver      = Account("","")
+    passenger   =str
+
+def __init__(self,license,driver):
+    self.license    = licence
+    self.driver     = driver
+    

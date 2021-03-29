2fba3f6dfc47445c51457f9c765d60618f17f8fe
@ppwicho <luis89@gmail.com>
2021-03-28 20:24:19 -0600

    agregamos js y corregimos duplicidad en UberX a uberX

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW555",new Account("Andres Herrera","AM55"))
 car.passenger=4;
 car.printDataCar();
+
+var UberX = new uberX("AW555",new Account("Andres X","AW555"),"Chevrolet","Spark")
+uberX.passenger=4;
+uberX.printDataCar();
